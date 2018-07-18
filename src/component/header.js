cf6fad6c6ee239e8c4090469836a4e27437a12f4
manoj1202 <manoj.appalaneni@gmail.com>
2018-07-18 16:18:04 -0500

    Adding Header Component

--- src/component/header.js
+++ src/component/header.js
@@ -30,7 +30,7 @@ const styles = {
         backgroundColor: "#F8F8F8",
         justifyContent: "center",
         alignItems: "center",
-        heigth : 60,
+        height : 60,
         paddingTop: 15
     }
 }
