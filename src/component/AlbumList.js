4294a5f1d79bf402962176647a40216824f6f3ef
manoj1202 <manoj.appalaneni@gmail.com>
2018-07-23 16:02:45 -0500

    adding axios

--- src/component/AlbumList.js
+++ src/component/AlbumList.js
@@ -3,14 +3,19 @@ import {View,Text} from "react-native"
 import axios from "axios"
 
 class AlbumList extends Component{
-    componentWillMount(){
+ state ={albums:[]};
+
+     componentWillMount(){
         axios.get("https://rallycode.herokuapp.com/api/music_albums")
-          .then(response => console.log(response))
+          .then(response => this.setState({albums: response.data}))
+    }
+    renderAlbums(){
+      return  this.state.albums.map(album => <Text key={album.title}>{albim.title}</Text>)
     }
     render(){
         return(
            <View>
-              <Text>Album List!!!!!!!!!</Text>
+              {this.renderAlbums}
            </View>
         )
     }
