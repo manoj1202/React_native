66f44769330059058bd13087d81d1a8fa4598d23
manoj1202 <manoj.appalaneni@gmail.com>
2018-07-24 16:41:08 -0500

    adding Scrolling

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {  Text, View, AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 import Header from "./src/component/header"
 import AlbumList from "./src/component/AlbumList"
 
 export default class App extends React.Component {
   render() {
     return (
-      <View >
+      <View style={{flex:1}} >
         <Header headerText={"Albums"} />
         <AlbumList />
       </View>
--- src/component/AlbumDetail.js
+++ src/component/AlbumDetail.js
@@ -4,31 +4,51 @@ import Card from "./card"
 import CardSection from "./CardSection"
 
 const AlbumDetail= ({album}) => {
-    const {title,artist,thumbnail_image} = album;
+    const {title,artist,thumbnail_image,image} = album;
     return(
        <Card>
             <CardSection>
-            <View>
-                <Image source={{uri:thumbnail_image }} />
+            <View style={styles.thumbnailContainerStyle}>
+                <Image stye={styles.thumbnailStyle} 
+                   source={{uri:thumbnail_image }} />
             </View>
-            <View style={style.headerContentStyle}>
-                <Text>{title}</Text>
+            <View style={styles.headerContentStyle}>
+                <Text style={styles.headerTextStyle}>{title}</Text>
                 <Text>{artist}</Text>
             </View>
+            </CardSection>
+            <CardSection>
+                <Image style={styles.imageStyle}
+                        source={{uri:image}} />
             </CardSection>
         </Card>
     )
 }
 
-const style={
+const styles={
     headerContentStyle:{
         flexDirection:"column",
         justifyContent:"space-around"
     },
+    headerTextStyle:{
+        fontSize:18
+    },
     thumbnailStyle:{
         height: 50,
         width : 50
+    },
+    thumbnailContainerStyle:{
+        justifyContent:"center",
+        alignItems:"center",
+        marginLeft:10,
+        marginRight:10
+    },
+    imageStyle:{
+        height:300,
+        flex:1,
+        width: null,
     }
+     
 }
 
 export default AlbumDetail
